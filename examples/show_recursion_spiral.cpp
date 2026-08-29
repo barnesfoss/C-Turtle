49d3ed822db9a2b88adb633535ddb0df58203f52
barnesfoss <256431510+barnesfoss@users.noreply.github.com>
2026-08-29 00:23:58 -0400

    Format examples

--- examples/show_recursion_spiral.cpp
+++ examples/show_recursion_spiral.cpp
@@ -1,4 +1,4 @@
-/* 
+/*
  * File:   show_recursion_spiral.cpp
  * Author: Jesse W. Walker
  */
@@ -7,25 +7,27 @@
 
 namespace ct = cturtle;
 
-void spiral(ct::Turtle& rt, int len) {
-    if (len > 0) {
-        rt.forward(len);
-        rt.right(90);
-        spiral(rt, len - 5);
-    }
+void spiral(ct::Turtle &rt, int len) {
+  if (len > 0) {
+    rt.forward(len);
+    rt.right(90);
+    spiral(rt, len - 5);
+  }
 }
 
-int main(int argc, char** argv) {
-    ct::TurtleScreen scr;
-    ct::Turtle rt(scr);
+int main(int argc, char **argv) {
+  ct::TurtleScreen scr;
+  ct::Turtle rt(scr);
 
-    scr.onclick([&](int x, int y) {
+  scr.onclick(
+      [&](int x, int y) {
         rt.penup();
         rt.goTo(x, y);
         rt.pendown();
         spiral(rt, 100);
-    }, ct::MOUSEB_LEFT);
+      },
+      ct::MOUSEB_LEFT);
 
-    scr.mainloop();
-    return 0;
+  scr.mainloop();
+  return 0;
 }
--- examples/show_undo.cpp
+++ examples/show_undo.cpp
@@ -1,4 +1,4 @@
-/* 
+/*
  * File:   show_undo.cpp
  * Author: Jesse W. Walker
  */
@@ -7,37 +7,37 @@
 
 namespace ct = cturtle;
 
-void draw_star(ct::Turtle& t){
-    unsigned int startStackSz = t.undobufferentries();
-    
-    t.back(25);
-    for(int i = 0; i < 5; i++){
-        t.forward(50);
-        t.right(144);
-    }
-    
-    unsigned int endStackSz = t.undobufferentries() - startStackSz;
-    
-    for(int i = 0; i < endStackSz; i++)
-        t.undo();
+void draw_star(ct::Turtle &t) {
+  unsigned int startStackSz = t.undobufferentries();
+
+  t.back(25);
+  for (int i = 0; i < 5; i++) {
+    t.forward(50);
+    t.right(144);
+  }
+
+  unsigned int endStackSz = t.undobufferentries() - startStackSz;
+
+  for (int i = 0; i < endStackSz; i++)
+    t.undo();
 }
 
-int main(int argc, char** argv) {
-    ct::TurtleScreen scr;
-    scr.tracer(0,0);
-    ct::Turtle turtle(scr);
-    
-    //Set the size of the undo buffer/stack to 1000 total entry capacity.
-    turtle.setundobuffer(1000);
-    
-    scr.onclick([&](int x, int y){
-       turtle.penup();
-       turtle.goTo(x,y);
-       turtle.pendown();
-       
-       draw_star(turtle);
-    });
-    
-    scr.mainloop();
-    return 0;
+int main(int argc, char **argv) {
+  ct::TurtleScreen scr;
+  scr.tracer(0, 0);
+  ct::Turtle turtle(scr);
+
+  // Set the size of the undo buffer/stack to 1000 total entry capacity.
+  turtle.setundobuffer(1000);
+
+  scr.onclick([&](int x, int y) {
+    turtle.penup();
+    turtle.goTo(x, y);
+    turtle.pendown();
+
+    draw_star(turtle);
+  });
+
+  scr.mainloop();
+  return 0;
 }
